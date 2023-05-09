3d5a09d69850ad56765a42e7994e3f836bfac0f3
Bastian Hofmann <mail@bastianhofmann.de>
2023-05-09 18:29:18 +0200

    Code change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
     console.log('Got http request.');
-    res.send('Hello world!\n');
+    res.send('Hello world\n');
 });
 
 app.listen(PORT, HOST);
